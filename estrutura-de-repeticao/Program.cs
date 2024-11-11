db6c49074af551d31a25a589f1eebbc2be95601a
gabgrabit <gabrielhenriquef11@outlook.com>
2024-11-11 14:32:09 -0400

    adicionado repeticao while

---
+++ estrutura-de-repeticao/Program.cs
@@ -0,0 +1,12 @@
+﻿// Estrutura de Repeticao while
+Random numeroAleatorio = new Random();
+int numero = numeroAleatorio.Next(1, 101);
+int contador = 0;
+Console.WriteLine($"Número Alvo: {numero}");
+// Loop que incrementa o contador ateele atingir o numero alvo
+while (contador <= numero)
+{
+    Console.WriteLine($"Contador: {contador}.");
+    contador++;
+}
+Console.WriteLine($"Contador antingiu o numero alvo: {contador}");
